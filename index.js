0114d05f075ca7d945ab9d0c1b1eef41c447f08b
Aakriti <github.com/aakriti-sethi>
2021-10-01 23:40:32 +0530

    Commit

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-const menuToggle = document.querySelector('.toggle')
-const showcase = document.querySelector('.showcase')
+// fetch("jumbo.json")
+// .then(response => response.json())
+// .then(data => {
+//     for(var i=0; i<data.length; i++){
+//         document.getElementById("1").innerHTML = data[0].title + "<br/>" + data[0].subTitle + "<br/>";
+//     }
+//     for(var i=0; i<data.length; i++){
+//         document.getElementById("2").innerHTML = data[1].title + "<br/>" + data[1].subTitle + "<br/>";
+//     }
+//     for(var i=0; i<data.length; i++){
+//         document.getElementById("3").innerHTML = data[2].title + "<br/>" + data[2].subTitle + "<br/>";
+//     }
+//     // console.log(data.title);
+    
+//     // document.getElementById("1").innerHTML = data.title;
+// })
 
-        menuToggle.addEventListener('click', function(){
-            menuToggle.classList.toggle('active')
-            showcase.classList.toggle('active')
-        })
+fetch("jumbo.json")
+.then(function (response) {
+    return response.json();
+})
+.then(function (data) {
+    appendData(data);
+})
+.catch(function (err) {
+    console.log('error: ' + err);
+});
+function appendData(data) {
+    var mainContainer = document.getElementById("myData");
+    for (var i = 0; i < data.length; i++) {
+        var div = document.createElement("div");
+        div.innerHTML = data[i].title + '<br/><br/>' + data[i].subTitle + '<br/><br/>' ;
+        mainContainer.appendChild(div);
+    }
+}
+
